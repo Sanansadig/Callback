9576a4e9cbec325992edbb3c1902bae456f74800
Sanansadig <sanan.sadig@hotmail.com>
2017-03-31 18:20:36 +0400

    callback

---
+++ javas/script.js
@@ -0,0 +1,30 @@
+var obyekt = document.getElementById("obyekt");
+var sekil = document.getElementById("sekil");
+var a= "5px solid black";
+var borderless ="none";
+
+obyekt.addEventListener("click", function (){
+    border(a, 2000, bigger, backborder)
+})
+
+function border(a, second, callback, backborder) {
+    var settime = setTimeout( function() { 
+        obyekt.style.border=a;
+    }, second)
+    callback(400, 400, 5000, backborder);
+    clear();
+}
+
+function bigger (width, height, new_second, backborder) {
+    var settime = setTimeout(
+    function(){
+        sekil.style.width = width +"px";
+        sekil.style.height = height + "px";
+    }, new_second)
+    backborder(borderless, 50, 50)
+}
+function backborder(borderless, width, height) {
+        obyekt.style.border = borderless;
+        sekil.style.width = width +"px";
+        sekil.style.height = height + "px";
+}
